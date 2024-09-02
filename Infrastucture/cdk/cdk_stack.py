d6825787198cd09e4d84995ea64d251cb2dfda27
TruptiAB <165988003+TruptiAB@users.noreply.github.com>
2024-09-02 21:52:10 +0530

    added crawler

--- Infrastucture/cdk/cdk_stack.py
+++ Infrastucture/cdk/cdk_stack.py
@@ -40,22 +40,22 @@ class DemoStack(Stack):
             )
         )
 
-        # # Create a Glue Crawler
-        # crawler = aws_glue.CfnCrawler(self, "MyCrawler",
-        #     role="arn:aws:iam::123456789012:role/service-role/AWSGlueServiceRole",  # Replace with your IAM role ARN
-        #     database_name=database.ref,
-        #     targets=aws_glue.CfnCrawler.TargetsProperty(
-        #         s3_targets=[aws_glue.CfnCrawler.S3TargetProperty(
-        #             path=f"s3://{bucket.bucket_name}/"
-        #         )]
-        #     ),
-        #     table_prefix="my_",
-        #     schema_change_policy=aws_glue.CfnCrawler.SchemaChangePolicyProperty(
-        #         update_behavior="UPDATE_IN_DATABASE",
-        #         delete_behavior="DEPRECATE_IN_DATABASE"
-        #     ),
-        #     crawler_security_configuration=None,
-        #     configuration=None,
-        #     description="My Glue Crawler"
-        # )
+        # Create a Glue Crawler
+        crawler = aws_glue.CfnCrawler(self, "MyCrawler",
+            role=crawler_role.arn,  # Replace with your IAM role ARN
+            database_name=database.ref,
+            targets=aws_glue.CfnCrawler.TargetsProperty(
+                s3_targets=[aws_glue.CfnCrawler.S3TargetProperty(
+                    path=f"s3://{bucket.bucket_name}/"
+                )]
+            ),
+            table_prefix="my_",
+            schema_change_policy=aws_glue.CfnCrawler.SchemaChangePolicyProperty(
+                update_behavior="UPDATE_IN_DATABASE",
+                delete_behavior="DEPRECATE_IN_DATABASE"
+            ),
+            crawler_security_configuration=None,
+            configuration=None,
+            description="My Glue Crawler"
+        )
 
