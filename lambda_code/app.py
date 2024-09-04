a3a20a02d1959fb13f6c6608bb41fef594916de1
TruptiAB <165988003+TruptiAB@users.noreply.github.com>
2024-09-04 20:10:54 +0530

    added lamda to trigger the crwler

--- lambda_code/app.py
+++ lambda_code/app.py
@@ -1,6 +1,8 @@
 import boto3
 
-def lambda_handler(event, context):
+def handler(event, context):
     client = boto3.client('glue')
     response = client.start_crawler(Name='{crawler.ref}')
     return response
+
+
