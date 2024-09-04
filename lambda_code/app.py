020078a5c82c067430b42ae26af23c6e0f7ca1b9
TruptiAB <165988003+TruptiAB@users.noreply.github.com>
2024-09-04 23:17:20 +0530

    added for loop for glue tables

--- lambda_code/app.py
+++ lambda_code/app.py
@@ -2,7 +2,7 @@ import boto3
 import os
 
 def handler(event, context):
-    crawler_name_list  = os.environ.get('crawler_name_json')
+    crawler_name_list  = os.environ.get('GLUE_CRAWLER_NAME')
     client = boto3.client('glue')
     for crawler_name in crawler_name_list:
         response = client.start_crawler(Name=crawler_name)
