4165be2fa4ff24890d0a0a26e39388cb30c6d69c
TruptiAB <165988003+TruptiAB@users.noreply.github.com>
2024-09-04 23:44:42 +0530

    code changes

--- lambda_code/app.py
+++ lambda_code/app.py
@@ -1,9 +1,11 @@
 import boto3
 import os
+import json
 
 def handler(event, context):
-    crawler_name_list  = os.environ.get('GLUE_CRAWLER_NAME')
-    client = boto3.client('glue')
+    crawler_name_list = os.environ.get('GLUE_CRAWLER_NAME')
+    client = boto3.client('glue', region_name='us-east-1')
+    crawler_name_list = json.loads(crawler_name_list)
     for crawler_name in crawler_name_list:
         response = client.start_crawler(Name=crawler_name)
         return response
