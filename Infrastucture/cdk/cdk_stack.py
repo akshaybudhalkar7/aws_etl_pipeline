09ab09fe2c76f4314f83d5abb4e741d2a75d5a01
TruptiAB <165988003+TruptiAB@users.noreply.github.com>
2024-09-04 15:20:24 +0530

    added glue script

--- Infrastucture/cdk/cdk_stack.py
+++ Infrastucture/cdk/cdk_stack.py
@@ -1,5 +1,5 @@
 from os import path
-from aws_cdk import (aws_s3, Stack, aws_lambda, aws_iam, Duration, aws_glue)
+from aws_cdk import (aws_s3, Stack, aws_lambda, aws_iam, Duration, aws_glue, aws_s3_assets as s3_assets)
 from constructs import Construct
 import os
 from os import path
@@ -16,46 +16,52 @@ class DemoStack(Stack):
             bucket_name="%s-s3" % id,
         )
 
-        # Create a Glue Database
-        database = aws_glue.CfnDatabase(self, "MyDatabase",
-            catalog_id=self.account,
-            database_input=aws_glue.CfnDatabase.DatabaseInputProperty(
-                name="etl_pipline"
-            )
-        )
+        # Upload the Glue job script to S3
+        script_asset = s3_assets.Asset(self, "GlueJobScript",
+                                       path="glue_jobs/glue_script.py"
+                                       )
 
-        # Create an IAM Role for Glue Crawler
-        crawler_role = aws_iam.Role(self, "GlueCrawlerRole",
-                                assumed_by=aws_iam.ServicePrincipal("glue.amazonaws.com"),
-                                managed_policies=[
-                                    aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSGlueServiceRole")
-                                ]
-                                )
+        #
+        # # Create a Glue Database
+        # database = aws_glue.CfnDatabase(self, "MyDatabase",
+        #     catalog_id=self.account,
+        #     database_input=aws_glue.CfnDatabase.DatabaseInputProperty(
+        #         name="etl_pipline"
+        #     )
+        # )
+        #
+        # # Create an IAM Role for Glue Crawler
+        # crawler_role = aws_iam.Role(self, "GlueCrawlerRole",
+        #                         assumed_by=aws_iam.ServicePrincipal("glue.amazonaws.com"),
+        #                         managed_policies=[
+        #                             aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSGlueServiceRole")
+        #                         ]
+        #                         )
+        #
+        # # Attach inline policy to allow the role to access the S3 bucket
+        # crawler_role.add_to_policy(
+        #     aws_iam.PolicyStatement(
+        #         actions=["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
+        #         resources=[bucket.bucket_arn, f"{bucket.bucket_arn}/*"]
+        #     )
+        # )
 
-        # Attach inline policy to allow the role to access the S3 bucket
-        crawler_role.add_to_policy(
-            aws_iam.PolicyStatement(
-                actions=["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
-                resources=[bucket.bucket_arn, f"{bucket.bucket_arn}/*"]
-            )
-        )
-
-        # Create a Glue Crawler
-        crawler = aws_glue.CfnCrawler(self, "MyCrawler",
-            role=crawler_role.role_arn,  # Replace with your IAM role ARN
-            database_name=database.ref,
-            targets=aws_glue.CfnCrawler.TargetsProperty(
-                s3_targets=[aws_glue.CfnCrawler.S3TargetProperty(
-                    path=f"s3://{bucket.bucket_name}/"
-                )]
-            ),
-            table_prefix="my_",
-            schema_change_policy=aws_glue.CfnCrawler.SchemaChangePolicyProperty(
-                update_behavior="UPDATE_IN_DATABASE",
-                delete_behavior="DEPRECATE_IN_DATABASE"
-            ),
-            crawler_security_configuration=None,
-            configuration=None,
-            description="My Glue Crawler"
-        )
+        # # Create a Glue Crawler
+        # crawler = aws_glue.CfnCrawler(self, "MyCrawler",
+        #     role=crawler_role.role_arn,  # Replace with your IAM role ARN
+        #     database_name=database.ref,
+        #     targets=aws_glue.CfnCrawler.TargetsProperty(
+        #         s3_targets=[aws_glue.CfnCrawler.S3TargetProperty(
+        #             path=f"s3://{bucket.bucket_name}/"
+        #         )]
+        #     ),
+        #     table_prefix="my_",
+        #     schema_change_policy=aws_glue.CfnCrawler.SchemaChangePolicyProperty(
+        #         update_behavior="UPDATE_IN_DATABASE",
+        #         delete_behavior="DEPRECATE_IN_DATABASE"
+        #     ),
+        #     crawler_security_configuration=None,
+        #     configuration=None,
+        #     description="My Glue Crawler"
+        # )
 
---
+++ glue_jobs/glue_script.py
@@ -0,0 +1,19 @@
+# glue_jobs/my_glue_job.py
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Your Glue job logic here
+
+job.commit()
